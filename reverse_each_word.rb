8e2ce594e7cb6e536fb7c4cf35fc1c3fd49fcb15
github username <github email address>
2018-05-09 22:22:39 -0700

    Changed #reverse_each_word to .collect method

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,3 @@
 def reverse_each_word(sentence)
-  new_sentence = []
-  sentence.split(" ").each do |word|
-    new_sentence.push(word.reverse)
-  end
-  new_sentence.join(" ")
+  sentence.split(" ").collect {|word| word.reverse}.join(" ")
 end
